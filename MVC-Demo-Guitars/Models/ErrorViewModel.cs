a6d6908b52f5c06a5c99bc3f1a23750d8e42847b
bdurushia <b.durushia@gmail.com>
2024-04-01 09:24:30 -0500

    init commit

---
+++ MVC-Demo-Guitars/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_Demo_Guitars.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
